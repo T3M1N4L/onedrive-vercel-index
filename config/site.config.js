555b9d1890dcfbaa90e6e77ea3e96a08da87aae8
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-01-21 19:21:45 -0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "T3RM1N4L's OneDrive",
+  title: "T3RM1N4L's Files",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Testvercel',
